8a620592b32010a7c4c00d843c71d311cc7d3fe8
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-05-29 17:58:45 +0200

    feat: add tailwind configuration + font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,23 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': 'hsl(245, 58%, 54%)',
+        'secondary': 'hsl(246, 67%, 75%)',
+        'accent': 'hsl(246, 77%, 64%)',
+        'tertiary': 'hsla(246, 67%, 75%, 0.3)',
+        'destructive': 'hsl(0, 100%, 36%)',
+        'other': 'hsla(240, 36%, 5%, 0.05)',
+        'background': 'hsl(0, 0%, 98%)',
+        'foreground': 'hsl(240, 36%, 5%)',
+        'muted': 'hsl(0, 0%, 38%)',
+        'input': 'hsl(216, 12%, 84%)',
+      },
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
