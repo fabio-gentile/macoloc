7bc9a21d7af798a90a44f4c897abda339d71bbce
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-05-31 23:24:33 +0200

    feat: (front) about us and latests offers home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
     "./node_modules/flowbite/**/*.js", // set up the path to the flowbite package
