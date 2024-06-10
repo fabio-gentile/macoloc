1de64b356457ac86f13955ea20766f254345e4d9
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-06-10 22:58:24 +0200

    feat: (front) account setting password help

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -93,6 +93,7 @@ class AccountType extends AbstractType
                 'hidden_icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="custom-toggle-password-icon" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M9.88 9.88a3 3 0 1 0 4.24 4.24m-3.39-9.04A10 10 0 0 1 12 5c7 0 10 7 10 7a13.2 13.2 0 0 1-1.67 2.68"/><path d="M6.61 6.61A13.5 13.5 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61M2 2l20 20"/></g></svg>',
                 'button_classes' => ['custom-toggle-password-button'],
                 'mapped' => false,
+                'help' => 'Votre mot de passe contenir au moins 6 caractères.'
             ])
         ;
     }
