267b9da8b25ea2072f9997dc303f5d8fde296491
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-06-16 19:18:31 +0200

    feat: (front) add housings and tenants

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\HousingRepository;
+use App\Repository\TenantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(
+        HousingRepository $housingRepository,
+        TenantRepository $tenantRepository
+    ): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'housings' => $housingRepository->findBy([], ['createdAt' => 'DESC'], 5),
+            'tenants' => $tenantRepository->findBy([], ['createdAt' => 'DESC'], 10),
+            'today' => new \DateTime(),
         ]);
     }
 }
